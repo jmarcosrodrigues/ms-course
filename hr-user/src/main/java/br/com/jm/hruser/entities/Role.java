40731a35ab8c2751699b408b0e54dd0db96df9cb
João Marcos <marcosrodrigues200@msn.com>
2020-10-27 22:30:15 -0300

    User role many to many associetion

---
+++ hr-user/src/main/java/br/com/jm/hruser/entities/Role.java
@@ -0,0 +1,20 @@
+package br.com.jm.hruser.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+}
